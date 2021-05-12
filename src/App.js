067abe70c0535e4e32fe4b6105e90a01e65fc9b8
eullerpeixoto <eullerz01@hotmail.com>
2021-05-12 16:14:44 -0300

    added ... on useStates

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const [red, setRed] = React.useState()
-  const [green, setGreen] = React.useState()
-  const [blue, setBlue] = React.useState()
+  const [red, setRed] = React.useState('...')
+  const [green, setGreen] = React.useState('...')
+  const [blue, setBlue] = React.useState('...')
 
   const redd = Math.floor(Math.random() * 255) + 1;
   const reddd = redd < 100 ? '0' + redd : redd
