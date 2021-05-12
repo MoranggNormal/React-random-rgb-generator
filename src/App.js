5aae945d0b35b425b922c08f8875f8e4ffc720e1
eullerpeixoto <eullerz01@hotmail.com>
2021-05-12 15:38:04 -0300

    finalized project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [red, setRed] = React.useState()
+  const [green, setGreen] = React.useState()
+  const [blue, setBlue] = React.useState()
+
+  const redd = Math.floor(Math.random() * 255) + 1;
+  const reddd = redd < 100 ? '0' + redd : redd
+
+  const greenn = Math.floor(Math.random() * 255) + 1;
+  const grennn = greenn < 100 ? '0' + greenn : greenn
+
+  const bluee = Math.floor(Math.random() * 255) + 1;
+  const blueee = bluee < 100 ? '0' + bluee : bluee
+
+  React.useEffect(() => {
+    setRed(reddd)
+    setGreen(grennn)
+    setBlue(blueee)
+  }, [])
+
+
+  function handleClick (){
+    setRed(reddd)
+    setGreen(grennn)
+    setBlue(blueee)
+    
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <button type="button" onClick={handleClick}>PLS GIMME A NEW COLOR!</button>
+      <span>Current color: rgb({red}, {green}, {blue})</span>
+    </section>
   );
 }
 
